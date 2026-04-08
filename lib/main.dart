cab7161255df0504974f1b2eac04672f39ff79cc
NerdSemProerd <emanuellrossetto@hotmail.com>
2026-04-08 11:43:24 -0300

    Finalizado TextFild e Botão de icone

--- lib/main.dart
+++ lib/main.dart
@@ -72,8 +72,63 @@ class GamePage extends StatelessWidget {
               children: [
                 for (var letter in guess) Tile(letter.char, letter.type),
               ],
-            )
+            ),
+          GuessInput(
+            onSubmitGuess: (String guess) {
+              print(guess);
+            }
+          )
         ],
       ), );
+  }
+}
+
+//Lembrete (pergunta); Para todo novo widget de textfild precisa de uma classe pra ele? 
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text);
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 5,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(35)),
+                )
+              ),
+              controller: _textEditingController,
+              autofocus: true,
+              focusNode: _focusNode,
+              onSubmitted: (String value) {
+                _onSubmit();
+              },
+            ),
+          )
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.arrow_circle_up),
+          onPressed: _onSubmit,
+        )
+      ],
+    );
   }
 }
